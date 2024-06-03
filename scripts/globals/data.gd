96a7bedd806bcf933a474f195cf790ee3ecb65e3
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2024-06-04 01:18:44 +0600

    Refactor self-contained mode code

--- scripts/globals/data.gd
+++ scripts/globals/data.gd
@@ -2,28 +2,28 @@ extends Node
 ## Handles saving and loading user settings.
 
 
-const RECOGNIZED_SELF_CONTAINED_MODE_FILE_NAMES: PackedStringArray = [
-	"_sc_",
+const SELF_CONTAINED_FILE_NAMES: PackedStringArray = [
 	"._sc_",
+	"_sc_",
 ]
 
+var self_contained_mode: bool = false
 var settings: Dictionary = Constants.DEFAULT_SETTINGS.duplicate(true)
-var settings_file_path: String = "user://"
 var removed_sections: int = 0
 var removed_section_keys: int = 0
 var invalid_section_keys: int = 0
 
 var _config: ConfigFile = ConfigFile.new()
+var _settings_path: String = "%ssettings.cfg"
 
 
 func _ready() -> void:
-	if not OS.has_feature("editor"):
-		var application_dir_path: String = "%s/" % OS.get_executable_path().get_base_dir()
-		for file_name in RECOGNIZED_SELF_CONTAINED_MODE_FILE_NAMES:
-			if FileAccess.file_exists(application_dir_path + file_name):
-				settings_file_path = application_dir_path
-				break
-	settings_file_path += "settings.cfg"
+	for file_name in SELF_CONTAINED_FILE_NAMES:
+		if FileAccess.file_exists(Utils.get_application_dir() + file_name):
+			self_contained_mode = true
+			break
+	
+	_settings_path = _settings_path % Utils.get_userdata_path()
 	
 	for section in settings.keys():
 		var section_keys: Dictionary = settings[section]
@@ -31,7 +31,7 @@ func _ready() -> void:
 			if typeof(section_keys[key]) == TYPE_STRING and (section_keys[key].begins_with("res://") or section_keys[key].begins_with("user://")):
 				section_keys[key] = ProjectSettings.globalize_path(section_keys[key])
 	
-	if _config.load(settings_file_path) == OK:
+	if _config.load(_settings_path) == OK:
 		# Unknown sections/section keys are removed so the settings file looks neater.
 		# Section keys with unexpected types are not loaded on startup.
 		for section in _config.get_sections():
@@ -59,4 +59,4 @@ func _notification(what: int) -> void:
 			var section_keys: Dictionary = settings[section]
 			for key in section_keys.keys():
 				_config.set_value(section, key, section_keys[key])
-		_config.save(settings_file_path)
+		_config.save(_settings_path)
